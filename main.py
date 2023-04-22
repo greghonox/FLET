764d78b40c79d3c41abb992a00479d08573efe13
Gregorio Honorato <greghono@gmail.com>
2023-04-22 19:59:39 -0300

    feat: adding widget

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import flet as ft
 
+
 def main(page: ft.Page):
-    t = ft.Text(value='Jesus esta voltando', color='yellow')
-    page.controls.append(t)
+    
+    def button_clicked(e):
+        print(f'click {e}')
+        page.add(ft.Text("Clicked!", size=100))
+        page.update()
+        
+    page.add(
+        ft.Row(
+            controls=[
+                ft.Text(f'A {x}') for x in range(10)
+            ] + [ft.ElevatedButton(text="Say my name!", on_click=button_clicked)]
+        )
+    )
     page.update()
 
 ft.app(target=main, view=ft.WEB_BROWSER, port=5000)
